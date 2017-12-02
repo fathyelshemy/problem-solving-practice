16d42a94a1f3f1d1f27185c8cc76e33c5eebb1ce
fathy <fathy@DESKTOP-1B0ODOL>
2017-12-02 16:51:08 +0200

    add new problem chess_tourney

---
+++ src/main/java/com/a/Chess_Tourney.java
@@ -0,0 +1,33 @@
+//package com.a;
+//
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class Chess_Tourney {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//		Scanner scan= new Scanner (System.in);
+//		int n=scan.nextInt();
+//		int n2[]=new int [2*n];
+//		for(int i=0;i<2*n;i++) {
+//			n2[i]=scan.nextInt();
+//		}
+//		int counter=0;
+//		Arrays.sort(n2);
+//		for(int i=0;i<n;i++ ) {
+//			if(n2[i]<=n2[n+i] && n>1) {
+//				counter++;
+//			}
+//		}
+//		if(counter==n) {
+//			System.out.println("YES");
+//		}
+//		else {
+//			System.out.println("NO");
+//		}
+//
+//	}
+//
+//}
