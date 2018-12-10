6173a3c96ad0e13318ab52129b3ae1c46f9a050f
fathyelshemy8 <fathyelshemy8@gmail.com>
2018-12-10 14:04:28 +0200

    solve the following problem Girl or Boy from the following links
    https://vjudge.net/contest/271539#problem/B
    https://codeforces.com/problemset/problem/236/A

---
+++ src/main/java/com/vJudge/contest1/Boy_or_Girl.java
@@ -0,0 +1,37 @@
+//package com.vJudge.contest1;
+//
+//import java.util.HashSet;
+//import java.util.Scanner;
+//import java.util.Set;
+//
+//public class Boy_or_Girl {
+//	
+//	private static String takeInputString() {
+//		Scanner scan = new Scanner(System.in);
+//		String input=scan.next();
+//		return input;
+//	}
+//
+//	private static int findBoyOrGirl(String input) {
+//		char[] charInput= input.toCharArray();
+//		Set<String> hashSet= new HashSet<>();
+//		for(char c:charInput) {
+//			hashSet.add(String.valueOf(c));
+//		}
+//		int distinctChars=hashSet.size();
+//		return distinctChars;
+//	}
+//	private static void printBoyOfGirl(int numOfDistinct) {
+//		if (numOfDistinct%2==0) {
+//			System.out.println("CHAT WITH HER!");
+//		}else {
+//			System.out.println("IGNORE HIM!");
+//		}
+//	}
+//	public static void main(String[] args) {
+//		String input=takeInputString();
+//		int numOfDistinct=findBoyOrGirl(input);
+//		printBoyOfGirl(numOfDistinct);
+//	}
+//
+//}
