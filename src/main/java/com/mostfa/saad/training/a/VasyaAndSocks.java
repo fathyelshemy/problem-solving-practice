513f47dc33365625504b98f9334786494adfb06d
fathyelshemy8 <fathyelshemy8@gmail.com>
2019-01-07 13:29:04 +0200

    solve problem () from mostafaSaadSheet/a from the following link https://codeforces.com/contest/460/problem/A

---
+++ src/main/java/com/mostfa/saad/training/a/VasyaAndSocks.java
@@ -0,0 +1,20 @@
+//package com.mostfa.saad.training.a;
+//
+//import java.util.Scanner;
+//
+///**
+// * @author fathyelshemy
+// * @category mostafaSaadSheet/a
+// * @see https://codeforces.com/contest/460/problem/A
+// */
+//public class VasyaAndSocks {
+//
+//	public static void main(String[] args) {
+//		Scanner scan= new Scanner(System.in);
+//		int n=scan.nextInt();
+//		int m=scan.nextInt();
+//		int total=(int) (n+Math.floor((n-1)/(m-1)));
+//		
+//		System.out.println(total);
+//	}
+//}
