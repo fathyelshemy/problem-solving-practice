bb3f83c9fc51f65444e7e8a7480064b979c23f47
fathyelshemy8 <fathyelshemy8@gmail.com>
2019-01-11 16:52:22 +0200

    solve problem (Holidays) from mostafaSaadSheet/a from the following link
    https://codeforces.com/contest/670/problem/A

---
+++ src/main/java/com/mostfa/saad/training/a/Holidays.java
@@ -0,0 +1,49 @@
+//package com.mostfa.saad.training.a;
+//
+//import java.util.Scanner;
+//
+//public class Holidays {
+//
+//	public static void main(String[] args) {
+//
+//		Scanner scan= new Scanner(System.in);
+//		float n= scan.nextFloat();
+//		int min=0;
+//		int max=0;
+//		for(int i=1;i<=n;i++) {
+//			if(i==1 || i==2 || i-(5)==1 || i-(5)==2) {
+//				max++;
+//			}
+//			if(i==6 || i==7 || i-(5*(i/7))==6 || i-(5*(i/7))==7) {
+//				min++;
+//			}
+//		}
+//		
+//		System.out.println(min+" "+max);
+//
+//	}
+//
+//}
+//
+///*
+// * 	public static void main(String[] args) {
+//
+//		Scanner scan= new Scanner(System.in);
+//		float n= scan.nextFloat();
+//		float nOfWeeks=n/7;
+//		if(n>5) {
+//		int min=(int) ((int)(Math.round(nOfWeeks*1)/1)+(n/7));
+//		int max=(int) ((Math.round(nOfWeeks*1)/1)*2);
+//		System.out.println(min+" "+max);
+//		}else if(n>=2 && n<=5) {
+//			int min=(int) ((int)(Math.floor(nOfWeeks)));
+//			int max=(int) ((Math.ceil(nOfWeeks))*2);
+//			System.out.println(min+" "+max);
+//		}else if(n==1){
+//			System.out.println(0+" "+1);
+//		}else {
+//			System.out.println(0+" "+0);
+//		}
+//
+//	}
+// */
