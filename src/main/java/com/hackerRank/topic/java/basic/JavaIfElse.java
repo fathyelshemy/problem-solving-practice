cbb34797cf3f015dbc3240ee51be3809dc44505c
fathyelshemy8 <fathyelshemy8@gmail.com>
2019-02-09 17:19:40 +0200

    solve problem from hacker rank website for java basic

---
+++ src/main/java/com/hackerRank/topic/java/basic/JavaIfElse.java
@@ -0,0 +1,24 @@
+//package com.hackerRank.topic.java.basic;
+//
+//import java.util.Scanner;
+//
+//public class JavaIfElse {
+//
+//	public static void main(String[] args) {
+//
+//		Scanner scan= new Scanner(System.in);
+//		int N=scan.nextInt();
+//		scan.close();
+//		if(N%2==1) {
+//			System.out.println("Weird");
+//		}else if((N%2==0)&& (N>=2)&& (N<=5)) {
+//			System.out.println("Not Weird");
+//		}else if((N%2==0)&& (N>=6)&& (N<=20)) {
+//			System.out.println("Weird");
+//		}else if((N%2==0) && (N>20)) {
+//			System.out.println("Not Weird");
+//		}
+//	}
+//	
+//
+//}
