580616935cf305ee0e7f9b5c4c190ac44b4b9440
fathyelshemy8 <fathyelshemy8@gmail.com>
2019-03-09 01:29:32 +0200

    solve problem (Decoding) from mostafaSaadSheet/b

---
+++ src/main/java/com/mostfa/saad/training/b/Decoding.java
@@ -0,0 +1,43 @@
+//package com.mostfa.saad.training.b;
+//
+//import java.util.Scanner;
+//import java.util.TreeSet;
+//
+//public class Decoding {
+//
+//	public static void main(String[] args) {
+//
+//		Scanner scan= new Scanner(System.in);
+//		int size=Integer.parseInt(scan.nextLine());
+//		String encodingStr=scan.nextLine();
+//		char [] encoded= encodingStr.toCharArray();
+//		char [] decoded=new char[size];
+//		
+//		if(size%2==1) {
+//			int middle=(size+1)/2;
+//			decoded[middle-1]=encoded[0];
+//			for(int i=1;i<size;i++ ) {
+//				if(i%2==1) {
+//					middle=middle-i;
+//				}else {
+//					middle=middle+i;
+//				}
+//				decoded[middle-1]=encoded[i];
+//			}
+//		}else {
+//			int middle=size/2;
+//			decoded[middle-1]=encoded[0];
+//			for(int i=1;i<size;i++ ) {
+//				if(i%2==1) {
+//					middle=middle+i;
+//				}else {
+//					middle=middle-i;
+//				}
+//				decoded[middle-1]=encoded[i];
+//			}
+//		}
+//		System.out.println(new String(decoded));
+//		
+//	}
+//
+//}
